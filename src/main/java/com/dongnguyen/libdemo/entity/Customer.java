ee85c5f3561c1cd38684709558e53199d0ba5241
nguyentiendong99 <dong190699@gmail.com>
2022-07-17 16:25:21 +0700

    init project

---
+++ src/main/java/com/dongnguyen/libdemo/entity/Customer.java
@@ -0,0 +1,20 @@
+package com.dongnguyen.libdemo.entity;
+
+public class Customer {
+    private int age;
+
+    public Customer(int age) {
+        this.age = age;
+    }
+
+    public Customer() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
